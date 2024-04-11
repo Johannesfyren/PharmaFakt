e8437116851ccb8541f79829e7b0e727ef648879
JohannesFyren <hergaardjohannes@gmail.com>
2024-04-11 20:17:46 +0200

    Add slide in and out effect on sidepanel

---
+++ js.js
@@ -0,0 +1,16 @@
+const collapseNavButton = document.querySelector('.collapseButton');
+const navigationContainer = document.querySelector('.left-nav');
+let navClosed = false;
+
+
+collapseNavButton.addEventListener("click", (e) =>{
+    if (navClosed == false){
+        navigationContainer.setAttribute("style", "right:230px;width:0; transition:0.2s;");
+        collapseNavButton.setAttribute("style", "left:0; transition:0.2s;");
+        navClosed = true;
+    }else{
+        navigationContainer.setAttribute("style", "right:0px;width:230; transition:0.2s;");
+        collapseNavButton.setAttribute("style", "left:230; transition:0.2s;");
+        navClosed = false;
+    };
+});
