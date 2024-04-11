4658998f80f6742a29a2aee146cc6005eea62342
JohannesFyren <hergaardjohannes@gmail.com>
2024-04-11 20:48:45 +0200

    Add navigation closing/opening icon

--- js.js
+++ js.js
@@ -6,11 +6,12 @@ let navClosed = false;
 collapseNavButton.addEventListener("click", (e) =>{
     if (navClosed == false){
         navigationContainer.setAttribute("style", "right:230px;width:0; transition:0.2s;");
-        collapseNavButton.setAttribute("style", "left:0; transition:0.2s;");
+        collapseNavButton.setAttribute("style", "left:0; transition:0.2s; rotate:180deg;");
+        
         navClosed = true;
     }else{
         navigationContainer.setAttribute("style", "right:0px;width:230; transition:0.2s;");
-        collapseNavButton.setAttribute("style", "left:230; transition:0.2s;");
+        collapseNavButton.setAttribute("style", "left:230; transition:0.2s; rotate:0deg;");
         navClosed = false;
     };
 });
