fd28cda05528867769a61ca748a585cb2ec65eb1
JohannesFyren <hergaardjohannes@gmail.com>
2024-04-12 15:32:40 +0200

    Add js to create functionality to dispatch

--- dispatch.js
+++ dispatch.js
@@ -1,9 +1,29 @@
-const dispatchContainer = document.createElement("div");
-const createDispatchBtn = document.querySelector("#new-dispatch-btn");
 const body = document.querySelector("body");
+const createDispatchBtn = document.querySelector("#new-dispatch-btn");
+
+const dispatchContainer = document.createElement("div");
+const UpperSectionWrapper = document.createElement("div");
+const CustVetWrapper = document.createElement("div");
+const selectCustomer = document.createElement("select");
+const selectVet = document.createElement("select");
+const CustDetailsWrapper = document.createElement("div");
+const VetDetailsWrapper = document.createElement("div");
+
 dispatchContainer.setAttribute("class","dispatch-container");
+UpperSectionWrapper.setAttribute("id","custvet-detail-wrapper");
+CustVetWrapper.setAttribute("id","customer-vet-wrapper");
+CustDetailsWrapper.setAttribute("id","customer-details-wrapper");
+VetDetailsWrapper.setAttribute("id","vet-details-wrapper");
+selectCustomer.setAttribute("id","customers");
+selectVet.setAttribute("id","vets");
 
 createDispatchBtn.addEventListener("click", ()=>{
-    body.prepend(dispatchContainer);
-    console.log("pols");
+    body.append(dispatchContainer);
+    dispatchContainer.append(UpperSectionWrapper);
+    UpperSectionWrapper.append(CustVetWrapper);
+    UpperSectionWrapper.append(CustDetailsWrapper);
+    UpperSectionWrapper.append(VetDetailsWrapper);
+
+    CustVetWrapper.append(selectCustomer);
+    CustVetWrapper.append(selectVet);
 });
