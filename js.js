28e3cd927965559b4099d1e66883e1421b2e9941
JohannesFyren <hergaardjohannes@gmail.com>
2024-04-12 11:17:33 +0200

    Clean up js-files and add dispatch.js

--- js.js
+++ js.js
@@ -1,23 +1,6 @@
-const collapseNavButton = document.querySelector('.collapseButton');
-const navigationContainer = document.querySelector('.left-nav');
 const internalNoteContainer = document.querySelector('.internal-note-container');
 const addNoteButton = document.querySelector('#add-note-button');
-
 let navClosed = false;
-
-
-collapseNavButton.addEventListener("click", (e) =>{
-    if (navClosed == false){
-        navigationContainer.setAttribute("style", "right:230px;width:0; transition:0.2s;");
-        collapseNavButton.setAttribute("style", "left:0; transition:0.2s; rotate:180deg;");
-        
-        navClosed = true;
-    }else{
-        navigationContainer.setAttribute("style", "right:0px;width:230; transition:0.2s;");
-        collapseNavButton.setAttribute("style", "left:230; transition:0.2s; rotate:0deg;");
-        navClosed = false;
-    };
-});
 
 
 addNoteButton.addEventListener("click", (e)=>{
