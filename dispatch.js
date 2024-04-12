062838b3c21b2cca2571e921ce4c23c090e1579f
JohannesFyren <hergaardjohannes@gmail.com>
2024-04-12 17:04:01 +0200

    Add selector to choose customer

--- dispatch.js
+++ dispatch.js
@@ -1,29 +1,88 @@
+const customer = [{
+    name: "John Deer",
+    address: "Bakers Street 39",
+    deliverAddress: "Bakers Street 39",
+},
+{
+    name: "Kathrina Baker",
+    address: "Country Street 1",
+    deliverAddress: "Bakers Mainroad 39",
+},
+{
+    name: "Hans Hinterseer",
+    address: "South Avenue 2",
+    deliverAddress: "South Avenue 2",
+}];
+
 const body = document.querySelector("body");
 const createDispatchBtn = document.querySelector("#new-dispatch-btn");
 
 const dispatchContainer = document.createElement("div");
-const UpperSectionWrapper = document.createElement("div");
-const CustVetWrapper = document.createElement("div");
+const upperSectionWrapper = document.createElement("div");
+const custVetWrapper = document.createElement("div");
 const selectCustomer = document.createElement("select");
 const selectVet = document.createElement("select");
-const CustDetailsWrapper = document.createElement("div");
-const VetDetailsWrapper = document.createElement("div");
+const custDetailsWrapper = document.createElement("div");
+const vetDetailsWrapper = document.createElement("div");
+
+const customerName = document.createElement("p");
+const customerAddress = document.createElement("p");
+const customerDeliveryAddress = document.createElement("p");
+
+const vetName = document.createElement("p");
+const vetAddress = document.createElement("p");
+const vetAuthID = document.createElement("p");
 
 dispatchContainer.setAttribute("class","dispatch-container");
-UpperSectionWrapper.setAttribute("id","custvet-detail-wrapper");
-CustVetWrapper.setAttribute("id","customer-vet-wrapper");
-CustDetailsWrapper.setAttribute("id","customer-details-wrapper");
-VetDetailsWrapper.setAttribute("id","vet-details-wrapper");
+upperSectionWrapper.setAttribute("id","custvet-detail-wrapper");
+custVetWrapper.setAttribute("id","customer-vet-wrapper");
+custDetailsWrapper.setAttribute("id","customer-details-wrapper");
+vetDetailsWrapper.setAttribute("id","vet-details-wrapper");
 selectCustomer.setAttribute("id","customers");
+selectCustomer.setAttribute("name","customers");
 selectVet.setAttribute("id","vets");
 
 createDispatchBtn.addEventListener("click", ()=>{
-    body.append(dispatchContainer);
-    dispatchContainer.append(UpperSectionWrapper);
-    UpperSectionWrapper.append(CustVetWrapper);
-    UpperSectionWrapper.append(CustDetailsWrapper);
-    UpperSectionWrapper.append(VetDetailsWrapper);
-
-    CustVetWrapper.append(selectCustomer);
-    CustVetWrapper.append(selectVet);
+    createDispatchElements();
 });
+
+
+function createDispatchElements(){
+    body.append(dispatchContainer);
+
+    //first div with select elements
+    dispatchContainer.append(upperSectionWrapper);
+    upperSectionWrapper.append(custVetWrapper);
+    upperSectionWrapper.append(custDetailsWrapper);
+    upperSectionWrapper.append(vetDetailsWrapper);
+    custVetWrapper.append(selectCustomer);
+    getAllCustomerOptions();
+    custVetWrapper.append(selectVet);
+
+    //second div with p elements
+    custDetailsWrapper.append(customerName);
+    custDetailsWrapper.append(customerAddress);
+    custDetailsWrapper.append(customerDeliveryAddress);
+
+
+    //third div with p elements
+    vetDetailsWrapper.append(vetName);
+    vetDetailsWrapper.append(vetAddress);
+    vetDetailsWrapper.append(vetAuthID);
+}
+
+
+function getAllCustomerOptions(){
+    
+    for (let i = 0;i<customer.length;i++){
+        const createOption = document.createElement("option");
+        console.log(createOption);
+        createOption.textContent = customer[i].name;
+        console.log(createOption);
+        selectCustomer.append(createOption);
+        
+        
+        
+        // selectCustomer.append(createOption.nodeValue = `${customer[i].name}`);
+    }
+}
