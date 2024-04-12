f088a1a3bb035cabbb37a78d6004279cd64b294a
JohannesFyren <hergaardjohannes@gmail.com>
2024-04-12 10:34:23 +0200

    Add ability to add internal note

--- js.js
+++ js.js
@@ -1,5 +1,8 @@
 const collapseNavButton = document.querySelector('.collapseButton');
 const navigationContainer = document.querySelector('.left-nav');
+const internalNoteContainer = document.querySelector('.internal-note-container');
+const addNoteButton = document.querySelector('#add-note-button');
+
 let navClosed = false;
 
 
@@ -14,4 +17,30 @@ collapseNavButton.addEventListener("click", (e) =>{
         collapseNavButton.setAttribute("style", "left:230; transition:0.2s; rotate:0deg;");
         navClosed = false;
     };
+});
+
+
+addNoteButton.addEventListener("click", (e)=>{
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+     "July", "August", "September", "October", "November", "December"
+    ];
+    const currentDate = new Date;
+    const userInput = prompt("Write note..");
+    console.log(userInput);
+
+
+    if (userInput) {
+        const userInitials = "CU";
+        const noteContainer = document.createElement("div");
+        const noteDate = document.createElement("p");
+        const noteInput = document.createElement("p");
+
+        noteContainer.setAttribute("class", "internal-notes");
+        noteDate.textContent = `${currentDate.getUTCDate()}. ${monthNames[currentDate.getUTCMonth()]}`;
+        noteInput.textContent = `${userInitials}: ${userInput}`;
+        
+        internalNoteContainer.prepend(noteContainer);
+        noteContainer.append(noteDate);
+        noteContainer.append(noteInput);
+    }
 });
