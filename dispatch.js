4766600063550ef77cc3cb2d00394c85253d7c1a
JohannesFyren <hergaardjohannes@gmail.com>
2024-04-13 07:37:47 +0200

    Add vet dropdown to choose from vets object

--- dispatch.js
+++ dispatch.js
@@ -16,6 +16,24 @@ const customer = [{
     address: "South Avenue 2",
     deliverAddress: "South Avenue 2",
 }];
+const vet = [{
+    ID:1,
+    name: "Alicia Dog",
+    address: "Bakers Street 39",
+    authID: "3151",
+},
+{
+    ID:2,
+    name: "James Cat",
+    address: "Country Street 1",
+    authID: "5462",
+},
+{
+    ID:3,
+    name: "Catherine Parrot",
+    address: "South Avenue 2",
+    authID: "1290",
+}];
 
 const body = document.querySelector("body");
 const createDispatchBtn = document.querySelector("#new-dispatch-btn");
@@ -56,8 +74,16 @@ selectCustomer.addEventListener("change", (e)=>{
     customerDeliveryAddress.textContent = chosenCustomer[0].deliverAddress;
     custDetailsWrapper.append(customerName);
     custDetailsWrapper.append(customerAddress);
-    custDetailsWrapper.append(customerDeliveryAddress);
-    
+    custDetailsWrapper.append(customerDeliveryAddress); 
+});
+selectVet.addEventListener("change", (e)=>{
+    const chosenVet = getVetDetails(e.target.value);
+    vetName.textContent = chosenVet[0].name;
+    vetAddress.textContent = chosenVet[0].address;
+    vetAuthID.textContent = chosenVet[0].authID;
+    vetDetailsWrapper.append(vetName);
+    vetDetailsWrapper.append(vetAddress);
+    vetDetailsWrapper.append(vetAuthID); 
 });
 
 function createDispatchElements(){
@@ -69,8 +95,8 @@ function createDispatchElements(){
     upperSectionWrapper.append(custDetailsWrapper);
     upperSectionWrapper.append(vetDetailsWrapper);
     custVetWrapper.append(selectCustomer);
-    getAllCustomerOptions();
     custVetWrapper.append(selectVet);
+    getAllCustVetOptions();
 
     //second div with p elements
     custDetailsWrapper.append(customerName);
@@ -84,18 +110,28 @@ function createDispatchElements(){
     vetDetailsWrapper.append(vetAuthID);
 }
 
-function getAllCustomerOptions(){
+function getAllCustVetOptions(){
     
-    const createOption = document.createElement("option");
-    selectCustomer.append(createOption); //Create blank, so list is not prefilled with info
+    const createOption1 = document.createElement("option");
+    const createOption2 = document.createElement("option");
+    selectCustomer.append(createOption1); //Create blank, so list is not prefilled with info
+    selectVet.append(createOption2); //Create blank, so list is not prefilled with info
 
     for (let i = 0;i<customer.length;i++){
         const createOption = document.createElement("option");
         createOption.textContent = customer[i].name;
         selectCustomer.append(createOption); 
     }
+    for (let i = 0;i<vet.length;i++){
+        const createOption = document.createElement("option");
+        createOption.textContent = vet[i].name;
+        selectVet.append(createOption); 
+    }
 }
 
 function getCustomerDetails(customerName){
     return foundCustomer = customer.filter(customer => customer.name == customerName); 
+}
+function getVetDetails(vetName){
+    return foundVet = vet.filter(vet => vet.name == vetName); 
 }
